c2be72dc29b5eba158c4a43a450322068e575f07
Gang Zhuo <gang.zhuo@gmail.com>
2025-04-09 09:11:38 +0800

    move 'lua/config/plugins' to top directory 'lua/plugins'

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,7 +15,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- import plugins
-    { import = "config.plugins" },
+    { import = "plugins" },
   },
   defaults = {
     -- should plugins be lazy-loaded?
