6cbd9357fa187ca6f7e283d73a4086bd0a8b0b17
Gang Zhuo <gang.zhuo@gmail.com>
2025-09-05 11:02:47 +0800

    disable copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,7 +3,7 @@ return {
   -- Github Copilot Completion
   {
     "zbirenbaum/copilot.lua",
-    enabled = true,
+    enabled = false,
     cmd = "Copilot",
     event = "InsertEnter",
     opts = {
