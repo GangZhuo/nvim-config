fe34b3dfccf56bf3fac9397a4755555026dc2d90
Gang Zhuo <gang.zhuo@gmail.com>
2025-11-07 09:31:31 +0800

    Update 'proxy.lua'

--- lua/config/proxy.lua
+++ lua/config/proxy.lua
@@ -1,37 +1,57 @@
 -- Try auto-configuring a proxy if it is not in the environment
 local env = vim.env
 local proxy = nil
+local no_proxy = nil
+
+local function zstr(s)
+  return s == nil or s == ""
+end
 
 local function get_proxy()
   if proxy then return proxy end
   local factory = function()
     local host = env.HPROXY_HOST
     local port = env.HPROXY_PORT
-    local zstr = function(s) return s == nil or s == "" end
     if not (zstr(host) or zstr(port)) then
       return string.format("http://%s:%s", host, port)
     end
     return nil
   end
-  proxy = env.http_proxy or env.https_proxy or factory() or ""
+  proxy = factory() or ""
   return proxy
+end
+
+local function get_no_proxy()
+  if no_proxy then return proxy end
+  no_proxy = "localhost"..
+    ",*.lan"..
+    ",127.0.0.1/8"..
+    ",10.0.0.0/8"..
+    ",192.168.0.0/16"..
+    ",169.254.0.0/16"..
+    ",100.64.0.0/10"..
+    ",fe80::/10"
+  return no_proxy
 end
 
 local function set_proxy()
   for _,var in ipairs({
     "http_proxy",
     "https_proxy",
+    "HTTP_PROXY",
+    "HTTPS_PROXY",
   }) do
-    if not env[var] then
+    if zstr(env[var]) then
       env[var] = get_proxy()
     end
   end
-  if not env.no_proxy then
-    local no_proxy = "localhost,127.0.0.1"
-    if env.IP4_GW then
-     no_proxy = no_proxy .. "," .. env.IP4_GW
+  for _,var in ipairs({
+    "no_proxy",
+    "NO_PROXY",
+  }) do
+    if zstr(env[var]) then
+      env[var] = get_no_proxy()
     end
-    env.no_proxy = no_proxy
   end
 end
 
@@ -39,12 +59,15 @@ local function unset_proxy()
   for _,var in ipairs({
     "http_proxy",
     "https_proxy",
+    "no_proxy",
+    "HTTP_PROXY",
+    "HTTPS_PROXY",
+    "NO_PROXY",
   }) do
     if env[var] then
       env[var] = nil
     end
   end
-  env.no_proxy = nil
 end
 
 return {
